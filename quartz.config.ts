9d40fd42383d32b8900ace2eb8f52626c8770f31
Fawzy-Moawad <fawzy3arafa88@gmail.com>
2026-03-22 09:52:52 -0700

    Quartz sync: Mar 22, 2026, 9:52 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "fawzy-moawad.github.io/Sufism",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
